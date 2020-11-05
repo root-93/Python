fa04be56e6ef1c144debb33e29279d3da86c7277
root93 <piotrq13@gmail.com>
2020-11-05 13:17:26 +0100

    update scale script

--- Scripts/scaleImages.py
+++ Scripts/scaleImages.py
@@ -3,37 +3,54 @@ from PIL import Image
 import os
 import pathlib
 
-
-
 print("Start scaling picture...")
-
+mainPath = os.getcwd()
+car = mainPath.split('/')[-1]
 maxSize = (1920, 1080)
-path = os.getcwd()
-car = path.split()[-1]
-#sufix = path.split()[-1]
 
-i = 1
-fileList = os.listdir(path)
-for fileName in fileList:
-    print(fileName)
-    
-    if(os.path.isdir(fileName)):
-        os.chdir(fileName)
-        imageList = os.listdir(os.getcwd())
-        print(fileName, " - is a dir")
-    else:
-        print(fileName, " - is not a dir")
-        continue
-    
-    for imageName in imageList:
-        print(imageName)
-        os.chdir('../')
-        # inputPath = os.path.join(path, dirName, imageName)
-        # newName = car + "_" + str(i) + ".JPG"
-        # i+=1
-        # outputPath = os.path.join(path, newName)
-        # print("input = ", inputPath)
-        # print("output = ", outputPath)
-        # image = Image.open(inputPath)
-        # image.thumbnail(maxSize, Image.ANTIALIAS)
-        # image.save(outputPath)
+def removeOld():   
+    for fileName in os.listdir(mainPath):
+        localPath = os.path.join(mainPath,fileName)
+        if(os.path.isdir(localPath)):
+            os.chdir(localPath)
+        else:
+            continue
+        for imageName in os.listdir(os.getcwd()):
+            if car in imageName:
+                os.remove(imageName)
+        os.chdir(mainPath)
+
+
+def scaleImages():
+    i = 1
+    for fileName in os.listdir(mainPath):
+        localPath = os.path.join(mainPath,fileName)
+
+        if(os.path.isdir(localPath)):
+            os.chdir(localPath)
+        else:
+            continue
+        
+        for imageName in os.listdir(os.getcwd()):
+            inputPath = os.path.join(localPath, imageName)
+            newName =car + "_" + fileName + "_" + str(i) + ".JPG"
+            i+=1
+            outputPath = os.path.join(localPath, newName)
+            print("input = ", inputPath)
+            print("output = ", outputPath)
+            image = Image.open(inputPath)
+            image.thumbnail(maxSize, Image.ANTIALIAS)
+            image.save(outputPath)
+        
+        i = 1
+        os.chdir(mainPath)
+
+
+#def makeSlideShowImages():
+    # soon...
+
+#---- main function ----------------
+
+removeOld()
+scaleImages()
+#makeSlideShowImages()
